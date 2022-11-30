dec300e9a6c8944eef8445a08ca0d3d8fa2764d5
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-30 19:06:48 +0300

    Redirect on click of the link works

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -14,6 +14,12 @@ class ResourcesController < ApplicationController
     end
   end
 
+  def bounce
+    @resource = Resource.find_by_short_url(params["short_url"])
+    puts "sdfjsldkfjsldkfjsldkfsdfsdf: #{@resource.long_url}"
+    redirect_to @resource.long_url, allow_other_host: true
+  end
+
   private
 
     def resource_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'resources#index'
 
   post '/resources/create', to: 'resources#create', as: 'resources_create'
+  get '/url/:short_url', to: 'resources#bounce', as: 'resources_bounce'
 end
