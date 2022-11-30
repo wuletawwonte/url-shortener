827e1c50c71960b65643cbb7b877f3234303b5c2
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-30 18:50:59 +0300

    Make the created short url appear in the homepage using turbo frames

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,23 @@
 class ResourcesController < ApplicationController
   def index
-    @resources = Resource.all
+    @resources = Resource.all.order(created_at: :desc)
   end
+
+  def create
+    @resource = Resource.new(resource_params)
+
+    if @resource.save
+      @resources = Resource.all.order(created_at: :desc)
+      render partial: "resources", locals: { resources: @resources }, notice: 'Successfully added.'
+    else
+      redirect_to :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def resource_params
+      params.permit(:long_url)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'resources#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/resources/create', to: 'resources#create', as: 'resources_create'
 end
