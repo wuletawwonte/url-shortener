fede2f293e70e71db15e2f0b3a06a7bcfe7695c0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-12-01 12:50:30 +0300

    Reset short_url field on toggle

--- app/javascript/controllers/alias_controller.js
+++ app/javascript/controllers/alias_controller.js
@@ -2,11 +2,12 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="alias"
 export default class extends Controller {
-  static targets = ['shorturlWrapper'];
+  static targets = ['shorturlWrapper', 'shorturlInput'];
 
   connect() {}
 
   toggleAlias() {
     this.shorturlWrapperTarget.classList.toggle('hidden');
+    this.shorturlInputTarget.value = '';
   }
 }
